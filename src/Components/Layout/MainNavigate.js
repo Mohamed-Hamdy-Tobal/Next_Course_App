aef4a187bdf36a5c9faac51c8f25d172a0f057d5
Mohamed Tobal <mohamedtobal369@gmail.com>
2023-12-25 10:24:36 +0200

    UP

---
+++ src/Components/Layout/MainNavigate.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const MainNavigate = () => {
+    return (
+        <header className="main-navigate bg-blue-950 flex justify-between items-center py-4 px-6 text-white text-[18px]">
+            <Link href='/' className="font-bold">Products</Link>
+            <Link href='/' className="font-bold">Add New Product</Link>
+        </header>
+    )
+}
+
+export default MainNavigate
